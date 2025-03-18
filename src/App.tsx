b466a31df63bf752419f33d324fe3ecbabd1cf3d
cerebrum101 <myusername22.05@gmail.com>
2025-03-18 23:52:29 +0500

    fixed onconnect

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ export default function App() {
       edges={edges}
       edgeTypes={edgeTypes}
       onEdgesChange={onEdgesChange}
-      // onConnect={onConnect}
+      onConnect={onConnect}
       fitView
       nodesDraggable={true}
       
